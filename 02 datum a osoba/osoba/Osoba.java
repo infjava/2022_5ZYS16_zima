b7b9cd548e9547b12b7e6b7c07e3bca5d672ac11
Jan Janech <jan.janech@fri.uniza.sk>
2022-09-30 12:25:34 +0200

    02 - uml model a vygenerovany kod pre projekt osoba

---
+++ 02 datum a osoba/osoba/Osoba.java
@@ -0,0 +1,29 @@
+public class Osoba {
+    private String meno;
+    private String priezvisko;
+    private String datumNarodenia;
+    
+    public Osoba(String meno, String priezvisko, String datumNarodenia) {
+        
+    }
+    
+    public String getMeno() {
+        
+    }
+    
+    public String getPriezvisko() {
+        
+    }
+    
+    public String getDatumNarodenia() {
+        
+    }
+    
+    public void setPriezvisko(String priezvisko) {
+        
+    }
+    
+    public void vypisDoTerminalu() {
+        
+    }
+}
