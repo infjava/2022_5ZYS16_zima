f12afab07e3c6780107529c1462cd6809992eb80
Jan Janech <jan.janech@fri.uniza.sk>
2022-09-30 12:32:00 +0200

    02 implementacia triedy Osoba

--- 02 datum a osoba/osoba/Osoba.java
+++ 02 datum a osoba/osoba/Osoba.java
@@ -4,26 +4,33 @@ public class Osoba {
     private String datumNarodenia;
     
     public Osoba(String meno, String priezvisko, String datumNarodenia) {
-        
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+        this.datumNarodenia = datumNarodenia;
     }
     
     public String getMeno() {
-        
+        return this.meno;
     }
     
     public String getPriezvisko() {
-        
+        return this.priezvisko;
     }
     
     public String getDatumNarodenia() {
-        
+        return this.datumNarodenia;
     }
     
     public void setPriezvisko(String priezvisko) {
-        
+        this.priezvisko = priezvisko;
     }
     
     public void vypisDoTerminalu() {
-        
+        System.out.print("Meno: ");
+        System.out.print(this.meno);
+        System.out.print(" ");
+        System.out.println(this.priezvisko);
+        System.out.print("Dátum narodenia: ");
+        System.out.println(this.datumNarodenia);
     }
 }
