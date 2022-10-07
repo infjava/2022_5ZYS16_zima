40b0d405256d7260251a8d584cafe2cfd0c99e1d
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-07 11:44:09 +0200

    03 - postfixova kalkulacka

--- 03 kalkulacka/kalkulacka/Kalkulacka.java
+++ 03 kalkulacka/kalkulacka/Kalkulacka.java
@@ -1,25 +1,27 @@
 public class Kalkulacka {
+    private double operand;
     private double predchadzajucaHodnota;
     
     public Kalkulacka() {
+        this.operand = 0;
         this.predchadzajucaHodnota = 0;
     }
     
-    public void scitaj(double operand) {
-        this.predchadzajucaHodnota = this.predchadzajucaHodnota + operand;
+    public void scitaj() {
+        this.predchadzajucaHodnota = this.predchadzajucaHodnota + this.operand;
     }
     
-    public void odcitaj(double operand) {
-        this.predchadzajucaHodnota = this.predchadzajucaHodnota - operand;
+    public void odcitaj() {
+        this.predchadzajucaHodnota = this.predchadzajucaHodnota - this.operand;
     }
     
-    public void vynasob(double operand) {
-        this.predchadzajucaHodnota = this.predchadzajucaHodnota * operand;
+    public void vynasob() {
+        this.predchadzajucaHodnota = this.predchadzajucaHodnota * this.operand;
     }
     
-    public void vydel(double operand) {
-        if (operand != 0) {
-            this.predchadzajucaHodnota = this.predchadzajucaHodnota / operand;
+    public void vydel() {
+        if (this.operand != 0) {
+            this.predchadzajucaHodnota = this.predchadzajucaHodnota / this.operand;
         } else {
             System.out.println("Sorry, ale delit nulou nemozes");
         }
@@ -27,6 +29,10 @@ public class Kalkulacka {
     
     public double getVysledok() {
         return this.predchadzajucaHodnota;
+    }
+    
+    public void setOperand(double operand) {
+        this.operand = operand;
     }
     
     public void setPociatocneCislo(double pociatocneCislo) {
