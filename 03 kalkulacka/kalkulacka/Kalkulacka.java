875a7b34fc42e981591656d1cf3e8eb264ae0f65
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-07 10:58:30 +0200

    03 - projekt pre kalkulacku s vygenerovanou triedou

---
+++ 03 kalkulacka/kalkulacka/Kalkulacka.java
@@ -0,0 +1,30 @@
+public class Kalkulacka {
+    
+    public Kalkulacka() {
+        
+    }
+    
+    public void scitaj(double operand) {
+        
+    }
+    
+    public void odcitaj(double operand) {
+        
+    }
+    
+    public void vynasob(double operand) {
+        
+    }
+    
+    public void vydel(double operand) {
+        
+    }
+    
+    public double getVysledok() {
+        return 0;
+    }
+    
+    public void setPociatocneCislo(double pociatocneCislo) {
+        
+    }
+}
