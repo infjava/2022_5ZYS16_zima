0ec1cdeeb3c9810d70e4ee045cf2e00fa08e26c8
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-07 11:16:52 +0200

    03 - funkcna kalkulacka

--- 03 kalkulacka/kalkulacka/Kalkulacka.java
+++ 03 kalkulacka/kalkulacka/Kalkulacka.java
@@ -1,30 +1,31 @@
 public class Kalkulacka {
+    private double predchadzajucaHodnota;
     
     public Kalkulacka() {
-        
+        this.predchadzajucaHodnota = 0;
     }
     
     public void scitaj(double operand) {
-        
+        this.predchadzajucaHodnota = this.predchadzajucaHodnota + operand;
     }
     
     public void odcitaj(double operand) {
-        
+        this.predchadzajucaHodnota = this.predchadzajucaHodnota - operand;
     }
     
     public void vynasob(double operand) {
-        
+        this.predchadzajucaHodnota = this.predchadzajucaHodnota * operand;
     }
     
     public void vydel(double operand) {
-        
+        this.predchadzajucaHodnota = this.predchadzajucaHodnota / operand;
     }
     
     public double getVysledok() {
-        return 0;
+        return this.predchadzajucaHodnota;
     }
     
     public void setPociatocneCislo(double pociatocneCislo) {
-        
+        this.predchadzajucaHodnota = pociatocneCislo;
     }
 }
