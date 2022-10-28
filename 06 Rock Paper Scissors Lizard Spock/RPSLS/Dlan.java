17f52ae039402bc04e6f77054517e37a5070abb1
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-28 11:36:56 +0200

    06 - prechod na dalsie gesto v dlani

---
+++ 06 Rock Paper Scissors Lizard Spock/RPSLS/Dlan.java
@@ -0,0 +1,33 @@
+public class Dlan {
+    private Obrazok gesto;
+    private int aktualneGesto;
+    
+    public Dlan() {
+        this.gesto = new Obrazok("pics\\lizard.png");
+        this.gesto.zobraz();
+        
+        this.aktualneGesto = 0;
+    }
+    
+    public void zmenNaDalsieGesto() {
+        this.aktualneGesto = (this.aktualneGesto + 1) % 5;
+        
+        switch (this.aktualneGesto) {
+            case 0:
+                this.gesto.zmenObrazok("pics\\lizard.png");
+                break;
+            case 1:
+                this.gesto.zmenObrazok("pics\\paper.png");
+                break;
+            case 2:
+                this.gesto.zmenObrazok("pics\\rock.png");
+                break;
+            case 3:
+                this.gesto.zmenObrazok("pics\\scissors.png");
+                break;
+            case 4:
+                this.gesto.zmenObrazok("pics\\spock.png");
+                break;
+        }
+    }
+}
