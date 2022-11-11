d3791a0f19071f81ed2e87b0c841a43d5a1bdaca
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-11 11:51:50 +0100

    08 - sucet vektorov

--- 08 matice a vektory/Vektor.java
+++ 08 matice a vektory/Vektor.java
@@ -30,4 +30,22 @@ public class Vektor {
         
         return vysledok;
     }
+    
+    public Vektor sucet(Vektor druhyVektor) {
+        if (this.vektor.length != druhyVektor.size()) {
+            System.out.println("Vektory musia mat rovnaky rozmer");
+            return null;
+        }
+        
+        int[] vysledok = new int[this.vektor.length];
+        
+        for (int i = 0; i < this.vektor.length; i++) {
+            int prvokZPrveho = this.vektor[i];
+            int prvokZDruheho = druhyVektor.get(i);
+            
+            vysledok[i] = prvokZPrveho + prvokZDruheho;
+        }
+        
+        return new Vektor(vysledok);
+    }
 }
