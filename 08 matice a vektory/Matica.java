cc945519cd0a828ca1291e4752963c30941534a8
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-11 11:57:06 +0100

    08 - ine zadavanie matice

--- 08 matice a vektory/Matica.java
+++ 08 matice a vektory/Matica.java
@@ -1,11 +1,7 @@
 public class Matica {
     private int[][] matica;
     
-    public Matica(int sirka, int vyska) {
-        this.matica = new int[vyska][sirka];
-    }
-    
-    public void nastavPrvok(int stlpec, int riadok, int hodnota) {
-        this.matica[riadok][stlpec] = hodnota;
+    public Matica(int[][] matica) {
+        this.matica = matica;
     }
 }
