1298c8bdcae7a406bb2412d0b7a91db12d11b1f0
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-11 12:31:19 +0100

    08 - zobrazenie zasahu lode/vody na suradniciach

--- 09 lodicky/Displej.java
+++ 09 lodicky/Displej.java
@@ -1,5 +1,8 @@
 public class Displej {
+    private PolickoDispleja[][] policka;
+    
     public Displej(int sirka, int vyska, int sirkaPolicka) {
+        this.policka = new PolickoDispleja[vyska][sirka];
         for (int y = 0; y < vyska; y++) {
             for (int x = 0; x < sirka; x++) {
                 PolickoDispleja pd = new PolickoDispleja(
@@ -7,15 +10,16 @@ public class Displej {
                     y * sirkaPolicka,
                     sirkaPolicka
                 );
+                this.policka[y][x] = pd;
             }
         }
     }
     
     public void zobrazZasahLode(int x, int y) {
-        
+        this.policka[y][x].zobrazZasahLode();
     }
     
     public void zobrazZasahVody(int x, int y) {
-        
+        this.policka[y][x].zobrazZasahVody();
     }
 }
