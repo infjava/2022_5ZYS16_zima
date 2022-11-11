aed7c11fb7979f676e3961be0ad04de8edbef596
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-11 12:23:22 +0100

    08 - vykreslovanie policka

--- 09 lodicky/Displej.java
+++ 09 lodicky/Displej.java
@@ -1,6 +1,14 @@
 public class Displej {
     public Displej(int sirka, int vyska, int sirkaPolicka) {
-        
+        for (int y = 0; y < vyska; y++) {
+            for (int x = 0; x < sirka; x++) {
+                PolickoDispleja pd = new PolickoDispleja(
+                    x * sirkaPolicka,
+                    y * sirkaPolicka,
+                    sirkaPolicka
+                );
+            }
+        }
     }
     
     public void zobrazZasahLode(int x, int y) {
