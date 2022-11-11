14cfeee839829e353183cfa3fbb3ce225df19719
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-11 11:31:33 +0100

    08 - skalarny sucin vektorov

--- 08 matice a vektory/Vektor.java
+++ 08 matice a vektory/Vektor.java
@@ -4,4 +4,30 @@ public class Vektor {
     public Vektor(int[] prvky) {
         this.vektor = prvky;
     }
+    
+    public int get(int index) {
+        return this.vektor[index];
+    }
+    
+    public int size() {
+        return this.vektor.length;
+    }
+    
+    public int skalarnySucin(Vektor druhyVektor) {
+        if (this.vektor.length != druhyVektor.size()) {
+            System.out.println("Vektory musia mat rovnaky rozmer");
+            return 0;
+        }
+        
+        int vysledok = 0;
+        
+        for (int i = 0; i < this.vektor.length; i++) {
+            int prvokZPrveho = this.vektor[i];
+            int prvokZDruheho = druhyVektor.get(i);
+            
+            vysledok = vysledok + prvokZPrveho * prvokZDruheho;
+        }
+        
+        return vysledok;
+    }
 }
