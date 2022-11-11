12a95a44e0abf57e14a6a06816a2c526af1ca4ee
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-11 10:46:45 +0100

    08 - zadavanie prvkov vektora inym sposobom

--- 08 matice a vektory/Vektor.java
+++ 08 matice a vektory/Vektor.java
@@ -1,11 +1,7 @@
 public class Vektor {
     private int[] vektor;
     
-    public Vektor(int pocetPrvkov) {
-        this.vektor = new int[pocetPrvkov];
-    }
-    
-    public void nastavPrvok(int index, int hodnota) {
-        this.vektor[index] = hodnota;
+    public Vektor(int[] prvky) {
+        this.vektor = prvky;
     }
 }
