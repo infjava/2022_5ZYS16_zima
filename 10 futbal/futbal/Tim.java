608ba30f3c47f654c474a6c51ec70b865a774b47
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-25 10:45:47 +0100

    10 - zaklad triedy Tim

---
+++ 10 futbal/futbal/Tim.java
@@ -0,0 +1,23 @@
+public class Tim {
+    private Stat stat;
+    private String nazov;
+    private int pocetBodov;
+    
+    public Tim(Stat stat, String nazov) {
+        this.stat = stat;
+        this.nazov = nazov;
+        this.pocetBodov = 0;
+    }
+    
+    public Stat getStat() {
+        return this.stat;
+    }
+    
+    public String getNazov() {
+        return this.nazov;
+    }
+    
+    public int getPocetBodov() {
+        return this.pocetBodov;
+    }
+}
