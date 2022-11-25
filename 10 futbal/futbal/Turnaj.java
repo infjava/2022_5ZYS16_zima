fd2fb74888c8d3304f44f2ed249fe784fde63eda
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-25 12:01:38 +0100

    10 - vypisanie vysledkovej matice

--- 10 futbal/futbal/Turnaj.java
+++ 10 futbal/futbal/Turnaj.java
@@ -17,4 +17,20 @@ public class Turnaj {
             }
         }
     }
+    
+    public void vypisVysledky() {
+        System.out.format("%10s", "");
+        for (Tim tim : this.timy) {
+            System.out.format("%10s", tim.getNazov());
+        }
+        System.out.println();
+        
+        for (int riadok = 0; riadok < timy.length; riadok++) {
+            System.out.format("%10s", this.timy[riadok].getNazov());
+            for (int stlpec = 0; stlpec < timy.length; stlpec++) {
+                System.out.format("%10s", this.vysledky[riadok][stlpec]);
+            }
+            System.out.println();
+        }
+    }
 }
