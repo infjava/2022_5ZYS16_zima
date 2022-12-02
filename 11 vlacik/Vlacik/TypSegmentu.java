aeace8733f0a549b500e406899010bc928653990
Jan Janech <jan.janech@fri.uniza.sk>
2022-12-02 11:18:09 +0100

    11 - typ segmentu vie vratit obrazok

--- 11 vlacik/Vlacik/TypSegmentu.java
+++ 11 vlacik/Vlacik/TypSegmentu.java
@@ -1,6 +1,16 @@
 public enum TypSegmentu {
-    RUSEN,
-    VAGON_ZELENY,
-    VAGON_MODRY,
-    VAGON_CERVENY
+    RUSEN("vlak.png"),
+    VAGON_ZELENY("vagon03.png"),
+    VAGON_MODRY("vagon01.png"),
+    VAGON_CERVENY("vagon02.png");
+    
+    private String nazovObrazku;
+    
+    TypSegmentu(String nazovObrazku) {
+        this.nazovObrazku = nazovObrazku;
+    }
+    
+    public String getCestaKObrazku() {
+        return "pics/" + this.nazovObrazku;
+    }
 }
