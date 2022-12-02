4b389ac6d397c4d1eb68597066dec3956119d6e1
Jan Janech <jan.janech@fri.uniza.sk>
2022-12-02 11:07:03 +0100

    11 - smer vie vratit uhol otocenia obrazku

--- 11 vlacik/Vlacik/Smer.java
+++ 11 vlacik/Vlacik/Smer.java
@@ -2,5 +2,20 @@ public enum Smer {
     HORE,
     DOLE,
     VLAVO,
-    VPRAVO
+    VPRAVO;
+    
+    public int getUhol() {
+        switch (this) {
+            case HORE:
+                return 90;
+            case DOLE:
+                return 270;
+            case VLAVO:
+                return 0;
+            case VPRAVO:
+                return 180;
+            default:
+                return 0;
+        }
+    }
 }
