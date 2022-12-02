51e124641ae721320836e1817d1471267a73218b
Jan Janech <jan.janech@fri.uniza.sk>
2022-12-02 12:23:56 +0100

    11 - posun vlaku pomocou rekurzie

--- 11 vlacik/Vlacik/SegmentVlaciku.java
+++ 11 vlacik/Vlacik/SegmentVlaciku.java
@@ -2,6 +2,7 @@ public class SegmentVlaciku {
     private final TypSegmentu typ;
     private Pozicia pozicia;
     private final Obrazok obrazok;
+    private SegmentVlaciku pripojeny;
     
     public SegmentVlaciku(TypSegmentu typ, Pozicia pozicia) {
         this.typ = typ;
@@ -11,9 +12,18 @@ public class SegmentVlaciku {
         this.obrazok.posunZvisle(-100 + pozicia.getY() * 35);
         this.obrazok.zmenUhol(pozicia.getSmer().getUhol());
         this.obrazok.zobraz();
+        this.pripojeny = null;
+    }
+    
+    public void pripoj(SegmentVlaciku dalsi) {
+        this.pripojeny = dalsi;
     }
     
     public void posunSa(Smer smer) {
+        if (this.pripojeny != null) {
+            this.pripojeny.posunSa(this.pozicia.getSmer());
+        }
+        
         Pozicia nova = this.pozicia.posun(smer);
         
         this.obrazok.posunVodorovne((nova.getX() - this.pozicia.getX()) * 35);
