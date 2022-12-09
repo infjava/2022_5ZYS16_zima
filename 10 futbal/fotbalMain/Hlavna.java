fc8208aef8d17c2af965ecd66d58fb6e7cb083de
Jan Janech <jan.janech@fri.uniza.sk>
2022-12-09 12:32:06 +0100

    12 - prikaz registruk

--- 10 futbal/fotbalMain/Hlavna.java
+++ 10 futbal/fotbalMain/Hlavna.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hlavna {
     public static void main(String[] args) {
         Scanner vstup = new Scanner(System.in);
         ZoznamTimov zoznam = new ZoznamTimov();
+        ArrayList<String> registrovaneTimy = new ArrayList<>();
         String prikaz;
         do {
             System.out.println("Mozne prikazy:");
@@ -36,6 +38,16 @@ public class Hlavna {
                     
                     zoznam.pridajTim(new Tim(nazovTimu, krajinaTimu));
                     
+                    break;
+                case "registruj":
+                    System.out.print("Zadaj nazov noveho timu> ");
+                    nazovTimu = vstup.nextLine();
+                    registrovaneTimy.add(nazovTimu);
+                    System.out.println("Timy zapisane do turnaja:");
+                    for (String nazov : registrovaneTimy) {
+                        System.out.format("%s ", nazov);
+                    }
+                    System.out.println();
                     break;
             }
         } while (!prikaz.equals("ukonci"));
