c9974eff7f7961e9dee81917a848c65e6d40a960
Jan Janech <jan.janech@fri.uniza.sk>
2022-12-09 10:55:18 +0100

    12 - vlacik sa da pustat mimo BlueJ

---
+++ 11 vlacik/Vlacik/Hlavna.java
@@ -0,0 +1,9 @@
+import fri.shapesge.Manazer;
+
+public class Hlavna {
+    public static void main(String[] args) {
+        Vlacik vlacik = new Vlacik();
+        Manazer manazer = new Manazer();
+        manazer.spravujObjekt(vlacik);
+    }
+}
